9aec6b2c30543bacb9b5e0c445ae35a8b0eab9a1
Stremyguy <stremyguy@gmail.com>
2025-05-12 16:05:51 +0300

    Added gitignore

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ def main() -> None:
         "apikey": "8013b162-6b42-4997-9691-77b7074026e0",
         "geocode": toponym_to_find,
         "format": "json"}
-
+    
     response = requests.get(geocoder_api_server, params=geocoder_params)
 
     if not response:
